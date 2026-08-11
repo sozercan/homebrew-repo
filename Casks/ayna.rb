2c994d01ecee54fecc2f29adcfeba94119455f2c
Sertac Ozercan <sozercan@gmail.com>
2026-08-11 00:12:40 -0700

    chore: update ayna to v0.4.0
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- Casks/ayna.rb
+++ Casks/ayna.rb
@@ -1,14 +1,14 @@
 cask "ayna" do
-  version "0.3.0"
-  sha256 "53d871bb2be1b4874aa6bdb8400385356700481d49fb94417ae09d3b118e4a1a"
+  version "0.4.0"
+  sha256 "5ff82a1ef47af6af48238213b8890e8b523496accdd7911dedf946b6797b84e5"
 
   url "https://github.com/sozercan/ayna/releases/download/v#{version}/ayna-v#{version}.dmg"
   name "Ayna"
-  desc "Native macOS/iOS/watchOS ChatGPT client"
+  desc "Native agentic AI client built with SwiftUI"
   homepage "https://github.com/sozercan/ayna"
 
   auto_updates true
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "Ayna.app"
 
